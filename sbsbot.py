792e76b1653ef099ad4e62b53adfaa1743e9e7dc
Lolfaceer <78652658+Lolfaceer@users.noreply.github.com>
2021-12-27 19:12:05 +0800

    Add files via upload

---
+++ sbsbot.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import sbsscraper
+
+TOKEN = 'OTI0OTY0NzY3NTA3MDQ2NDMx.YcmOkQ.4MSwavpw120Lsi1PdDrgXLQT35k'
+
+bot = commands.Bot(command_prefix = '!b ', case_insensitive = True)
+
+@bot.event
+async def on_ready():
+    print("Bot ready")
+
+@bot.listen()
+async def on_message(message):
+    message.content = message.content.lower()
+    if message.author == bot.user:
+        return
+    if message.content.startswith("beep"):
+        msg = '{0.author.mention} boop'.format(message)
+        await message.channel.send(msg)
+
+@bot.command(name = 'ping')
+async def ping(ctx):
+    ping = round(bot.latency *1000)
+    await ctx.send(f'{ping}ms')
+
+@bot.command(aliases = ['etags', 'e6tags', 'et'])
+async def arrive(ctx, station, service=None):
+    station = str(station)
+    if service is None:
+        msg = sbsscraper.all_service(station)
+        await ctx.send(msg)
+    else:
+        service = str(service)
+        msg = sbsscraper.a_service(station, service)
+        await ctx.send(msg)
+
+bot.run(TOKEN)
